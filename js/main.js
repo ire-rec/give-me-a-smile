9f02da884fb0d64392d4748f8ba9ba77ae83da3b
ire-rec <i.recavarren@gmail.com>
2023-11-27 15:40:51 +0100

    add js selectors

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-
+"use strict";
+const h1 = document.querySelector(".h1-js");
+const happyFaceOption = document.querySelector(".optionHappyFace-js");
+const sadFaceOption = document.querySelector(".optionSadFace-js");
+const updateBtn = document.querySelector(".updateButton-js");
